7da8484f8c0d6f6f5c9fe40a2056356ff29a54cf
FerrerJorenz <ferrer.253634@dasmarinas.sti.edu.ph>
2023-11-23 20:38:57 +0800

    Updates from Jorenz
    added reject.php
    added getDog.php
    updated adoptPage.css
    updated adopt.php

--- APP_DEV/adopt.php
+++ APP_DEV/adopt.php
@@ -1,26 +1,38 @@
 <?php
 include_once('./includes/header.php');
+
+// Check if the user is logged in
 if (!isset($_SESSION["uID"])) {
     header("location: ./index.php?error=UserLoggedOut");
     exit();
 }
 ?>
-    <div id="left-panel">
-        <ul>
-            <li><a href="#"> <img class="logo-side" src="./image/account.png">Profile</a></li>
-            <li><a href="adopt.php"><img class="logo-side" src="./image/dog.png">Adopt</a></li>
-            <li><a href="message.php"><img class="logo-side" src="./image/email.png">Messages</a></li>
-            <li><a href="./includes/logout.php"><img class="logo-side" src="./image/logout.png">Logout</a></li>
-        </ul>
-    </div>
+
+<div id="left-panel">
+    <ul>
+        <li><a href="#"> <img class="logo-side" src="./image/account.png">Profile</a></li>
+        <li><a href="adopt.php"><img class="logo-side" src="./image/dog.png">Adopt</a></li>
+        <li><a href="message.php"><img class="logo-side" src="./image/email.png">Messages</a></li>
+        <li><a href="./includes/logout.php"><img class="logo-side" src="./image/logout.png">Logout</a></li>
+    </ul>
+</div>
 
 <div class="right-panel">
     <div id="dog-container">
         <div id="dog-card">
+<<<<<<< Updated upstream
             <img src="./image/dogies/igit.jpg" alt="Dog Image">
+=======
+            <!-- Default dog card -->
+            <img src="./image/defaultDoggo.png" alt="Default Dog Image">
+            <div class="dog-card__content">
+                <p class="dog-card__title">No Dog Available</p>
+                <p class="dog-card__description">Check back later to find more buddies!</p>
+            </div>
+>>>>>>> Stashed changes
         </div>
-    </div>
 
+<<<<<<< Updated upstream
     <div id="dog-buttons">
         <button class="reject-button">&#10006;</button>
         <button class="heart-button">&#10084;</button>
@@ -59,10 +71,87 @@ if (!isset($_SESSION["uID"])) {
                      
                     </div>
                 </form>
+=======
+        <div id="dog-buttons">
+            <button class="reject-button">&#10006;</button>
+            <button id="change-image" class="heart-button">&#10084;</button>
+            <button class="paw-button">&#128062;</button>
+        </div>
+
+        <div class="add-dog-form">
+            <button onclick="toggleForm()" class="toggle-button">Add Dog for Adoption</button>
+            <div class="add-dog-container" id="dogFormContainer">
+                <div class="add-dog-form-inner">
+                    <!-- Added exit button here -->
+                    <button onclick="toggleForm()" class="exit-button">Exit Form</button>
+
+                    <form action="./includes/upload.php" method="POST" id="dogForm" enctype="multipart/form-data">
+                        <!-- Your form inputs go here -->
+                        <div class="add-dog-buttons">
+                            <button type="submit" name="addDog">Add Dog</button>
+                        </div>
+                    </form>
+                </div>
+>>>>>>> Stashed changes
             </div>
         </div>
     </div>
+
+    <script>
+        function toggleForm() {
+            var dogFormContainer = document.getElementById("dogFormContainer");
+            dogFormContainer.style.display = (dogFormContainer.style.display === "none" || dogFormContainer.style.display === "") ? "block" : "none";
+        }
+
+        $(document).ready(function () {
+            loadDog(); // Load the initial dog on page load
+
+            $("#change-image").click(function () {
+                // Handle changing the image
+                loadDog();
+            });
+
+            $(".reject-button").click(function () {
+                rejectDog();
+            });
+        });
+
+        function loadDog() {
+            $.ajax({
+                type: 'POST',
+                url: './includes/getDog.php',
+                data: { cacheBuster: new Date().getTime() }, // Cache buster
+                success: function (response) {
+                    $("#dog-card").html(response);
+                },
+                error: function () {
+                    alert('Error loading dog.');
+                }
+            });
+        }
+
+        function rejectDog() {
+            var currentDogID = $("#dog-card img").data("dogid");
+
+            $.ajax({
+                type: 'POST',
+                url: './includes/reject.php',
+                data: { rejectDog: true, dogID: currentDogID },
+                success: function (response) {
+                    $("#dog-card").html(response);
+                },
+                error: function () {
+                    alert('Error rejecting dog.');
+                }
+            });
+        }
+    </script>
+
+    <?php
+    include_once('./includes/footer.php');
+    ?>
 </div>
+<<<<<<< Updated upstream
 
 <script>
 function toggleForm() {
@@ -74,3 +163,5 @@ function toggleForm() {
 <?php
 include_once('./includes/footer.php');
 ?>
+=======
+>>>>>>> Stashed changes
--- APP_DEV/dbusers.sql
+++ APP_DEV/dbusers.sql
@@ -3,13 +3,14 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 16, 2023, at 03:42 PM
+-- Generation Time: Nov 23, 2023 at 01:24 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -23,17 +24,104 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `tbldogs`
+--
+
+CREATE TABLE `tbldogs` (
+  `username` varchar(255) NOT NULL,
+  `dogID` int(100) NOT NULL,
+  `name` varchar(100) NOT NULL DEFAULT 'No Name Yet',
+  `breed` varchar(255) NOT NULL DEFAULT 'Unknown Breed',
+  `age` varchar(50) NOT NULL DEFAULT 'Undefined',
+  `description` varchar(500) NOT NULL DEFAULT 'No Description Given',
+  `weight` varchar(5) NOT NULL DEFAULT 'N/A',
+  `image` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tbldogs`
+--
+
+INSERT INTO `tbldogs` (`username`, `dogID`, `name`, `breed`, `age`, `description`, `weight`, `image`) VALUES
+('amiel', 1, 'nigger', 'husky', 'aw', 'adwda', '', '655edbbec0bf92.96462726.jpg'),
+('amiel', 2, 'Black Eye', 'aspin', '12', 'awdawd', '', 'blackEye.jpg'),
+('admin', 3, 'enz', 'ewan', '', 'nigga', '', 'enz.jpg'),
+('admin', 4, 'igit', 'aspin', '12', 'ewan', '', 'igit.jpg'),
+('admin', 5, 'pepper', 'shih tzu', '11', 'ewan', '', 'pepper.jpg'),
+('amiel', 6, 'awd', 'awd', 'aw', 'awd', '', '655ee40a7098d0.09578960.jpg'),
+('amiel', 7, 'awdawd', 'awdad', 'ad', 'dawdad', '', '655ee4b225a0b5.74442308.jpg'),
+('amiel', 8, 'awda', 'wdawd', 'aw', 'awdawd', '', '655ee4ef119623.17479117.jpg'),
+('amiel', 663, 'awd', 'awda', 'da', 'awd', '', '655ee54141df77.63123655.jpg'),
+('amiel', 664, 'awdawd', 'awdaw', 'aw', 'dada', '', '655eeb70c5aab8.18286175.png'),
+('amiel', 667, 'Black Eye', 'aspin', '', '', '22', '655f08c3c59893.31248353.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblfavorites`
+--
+
+CREATE TABLE `tblfavorites` (
+  `ownerUser` varchar(255) NOT NULL,
+  `dogName` varchar(100) NOT NULL,
+  `dogImage` varchar(255) NOT NULL,
+  `dogBreed` varchar(255) NOT NULL,
+  `dogAge` varchar(255) NOT NULL,
+  `dogDescription` varchar(255) NOT NULL,
+  `dogWeight` varchar(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tblfavorites`
+--
+
+INSERT INTO `tblfavorites` (`ownerUser`, `dogName`, `dogImage`, `dogBreed`, `dogAge`, `dogDescription`, `dogWeight`) VALUES
+('admin', 'nigger', '655edbbec0bf92.96462726.jpg', 'husky', 'aw', 'adwda', ''),
+('amiel', 'pepper', 'pepper.jpg', 'shih tzu', '11', 'ewan', ''),
+('amiel', 'enz', 'enz.jpg', 'ewan', '', 'nigga', ''),
+('amiel', 'igit', 'igit.jpg', 'aspin', '12', 'ewan', ''),
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblrejecteddogs`
+--
+
+CREATE TABLE `tblrejecteddogs` (
+  `username` varchar(255) NOT NULL,
+  `dogID` int(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tblrejecteddogs`
+--
+
+INSERT INTO `tblrejecteddogs` (`username`, `dogID`) VALUES
+('Renz', 1),
+('Renz', 2),
+('Renz', 3),
+('Renz', 4),
+('Renz', 5),
+('Renz', 6),
+('Renz', 7),
+('Renz', 8),
+('Renz', 663),
+('Renz', 664),
+('Renz', 667);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `tblusers`
 --
 
 CREATE TABLE `tblusers` (
-  `username` varchar(100) NOT NULL,
+  `username` varchar(255) NOT NULL,
   `password` varchar(100) NOT NULL,
   `city` varchar(100) NOT NULL,
   `fname` varchar(100) NOT NULL,
   `lname` varchar(100) NOT NULL,
-  `uID` int(11) NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`uID`)
+  `uID` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -46,54 +134,33 @@ INSERT INTO `tblusers` (`username`, `password`, `city`, `fname`, `lname`, `uID`)
 ('amielito', '$2y$10$A8mLzjRKlhgBQziU9V/Q4.xVP3Jy7F.w0vMthzfJV7qbCR2eDKf/e', 'Imus', 'Amiel Carhyl', 'Lapid', 3),
 ('admin', '$2y$10$jkwhkebfNcCvoOlsz1EnneENR5pb4tO0kmhRFwFnUCzNQtRGVnk8a', 'Imus', 'Amiel Carhyl', 'Lapid', 4),
 ('aaawd', '$2y$10$IdRQ6GhKYaewduYxPjdPUOZnERrnhFMq1EtM9hEXFmLpaGYQwXJla', 'Imus', 'Amiel Carhyl', 'Lapid', 5),
-('hArt06', '$2y$10$orprCUIzB6YcUpDh4qKRmujZCsNUUfmjNI8Rzn9iUOVXChn4vymnW', 'Caloocan', 'Nicole Heart', 'Mendoza', 6);
+('hArt06', '$2y$10$orprCUIzB6YcUpDh4qKRmujZCsNUUfmjNI8Rzn9iUOVXChn4vymnW', 'Caloocan', 'Nicole Heart', 'Mendoza', 6),
+('Renz', '$2y$10$A7okzTUUynmbuv8JiuQjdOLt9OVb9E/S98NGQ7tPsTwPcpvV04Izq', 'General Trias', 'Jorenz', 'Ferrer', 7);
 
 --
 -- Indexes for dumped tables
 --
 
 --
+-- Indexes for table `tbldogs`
+--
+ALTER TABLE `tbldogs`
+  ADD PRIMARY KEY (`dogID`),
+  ADD KEY `username` (`username`);
+
+--
+-- Indexes for table `tblrejecteddogs`
+--
+ALTER TABLE `tblrejecteddogs`
+  ADD PRIMARY KEY (`username`,`dogID`),
+  ADD KEY `FK_dog` (`dogID`);
+
+--
 -- Indexes for table `tblusers`
 --
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `tblusers`
---
 ALTER TABLE `tblusers`
-  MODIFY `uID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tbldogs`
---
-
-CREATE TABLE `tbldogs` (
-  `dogID` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(100) NOT NULL,
-  `image` varchar(255) NOT NULL,
-  PRIMARY KEY (`dogID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tblfavorites`
---
-
-CREATE TABLE `tblfavorites` (
-  `favoriteID` int(11) NOT NULL AUTO_INCREMENT,
-  `userID` int(11) NOT NULL,
-  `dogName` varchar(100) NOT NULL,
-  `dogImage` varchar(255) NOT NULL,
-  PRIMARY KEY (`favoriteID`),
-  KEY `userID` (`userID`),
-  CONSTRAINT `tblfavorites_ibfk_1` FOREIGN KEY (`userID`) REFERENCES `tblusers` (`uID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  ADD PRIMARY KEY (`uID`),
+  ADD UNIQUE KEY `username` (`username`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -103,14 +170,25 @@ CREATE TABLE `tblfavorites` (
 -- AUTO_INCREMENT for table `tbldogs`
 --
 ALTER TABLE `tbldogs`
-  MODIFY `dogID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+  MODIFY `dogID` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=668;
 
 --
--- AUTO_INCREMENT for table `tblfavorites`
+-- AUTO_INCREMENT for table `tblusers`
 --
-ALTER TABLE `tblfavorites`
-  MODIFY `favoriteID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+ALTER TABLE `tblusers`
+  MODIFY `uID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `tblrejecteddogs`
+--
+ALTER TABLE `tblrejecteddogs`
+  ADD CONSTRAINT `FK_dog` FOREIGN KEY (`dogID`) REFERENCES `tbldogs` (`dogID`),
+  ADD CONSTRAINT `FK_user` FOREIGN KEY (`username`) REFERENCES `tblusers` (`username`),
+  ADD CONSTRAINT `tblrejecteddogs_ibfk_1` FOREIGN KEY (`username`) REFERENCES `tblusers` (`username`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
